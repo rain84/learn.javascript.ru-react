113f457d6cd81713e669d5d71a4144d5089266dd
rain84 <rainy_sky@mail.ru>
2024-01-26 02:05:50 +0300

    refactor: ♻️ renamed top-level styles of the components into 'root'-name

--- src/components/footer/component.tsx
+++ src/components/footer/component.tsx
@@ -1,5 +1,5 @@
 import styles from './style.module.scss'
 
 export const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>
+  return <footer className={styles.root}>Footer</footer>
 }
