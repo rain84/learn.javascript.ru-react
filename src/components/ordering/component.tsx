0c8e632e64cc42c75bf7bbd2e094eee364a66671
rain84 <rainy_sky@mail.ru>
2024-01-26 02:00:24 +0300

    feat: ✨ added <Ordering> & <Button> & <Plus> & <Minus> components

---
+++ src/components/ordering/component.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx'
+import { useState } from 'react'
+import { Minus } from './buttons/minus'
+import { Plus } from './buttons/plus'
+import styles from './style.module.scss'
+
+export const Ordering = ({ max = 5, className, onChange }: TProps) => {
+  const [ordering, setOrdering] = useState(0)
+
+  const isDisabled = {
+    increase: ordering === max,
+    decrease: ordering === 0,
+  }
+
+  const decrease = () => {
+    if (isDisabled.decrease) return
+
+    const next = ordering - 1
+    setOrdering(next)
+    onChange?.(next)
+  }
+  const increase = () => {
+    if (isDisabled.increase) return
+
+    const next = ordering + 1
+    setOrdering(next)
+    onChange?.(next)
+  }
+
+  return (
+    <span className={clsx(styles.root, className)}>
+      <Minus onClick={decrease} disabled={isDisabled.decrease} />
+      <span className={styles.counter}>{ordering}</span>
+      <Plus onClick={increase} disabled={isDisabled.increase} />
+    </span>
+  )
+}
+
+type TProps = {
+  max?: number
+  className?: string
+  onChange?: (value: number) => void
+}
