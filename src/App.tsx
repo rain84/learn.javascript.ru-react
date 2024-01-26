bd0e5be863a5e331a4a6ed892016cc6b10fe64a2
rain84 <rainy_sky@mail.ru>
2024-01-26 03:42:57 +0300

    feat: <Tabs> integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Restaurants } from 'components/restaurants'
+import { Restaurant } from 'components/restaurant'
 import { Layout } from './components/layout/component'
 import { restaurants } from './constants/mock'
 import styles from './styles/app.module.scss'
+import { Tabs } from 'components/tabs'
 
 export const App = () => {
   return (
     <Layout className={styles.layout}>
-      <Restaurants restaurants={restaurants} />
+      <Tabs items={restaurants} className={styles.tabs}>
+        <div className={styles.tabItems__container}>
+          {restaurants.map(({ id, ...restaurant }, i) => (
+            <Tabs.Item key={id} index={i}>
+              <Restaurant {...restaurant} />
+            </Tabs.Item>
+          ))}
+        </div>
+      </Tabs>
     </Layout>
   )
 }
