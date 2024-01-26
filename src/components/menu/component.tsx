a977ed5f1c081f5d1122bf53253e24d629d4ceee
rain84 <rainy_sky@mail.ru>
2024-01-26 03:51:43 +0300

    refactor: 💄 updated styles

--- src/components/menu/component.tsx
+++ src/components/menu/component.tsx
@@ -7,7 +7,7 @@ export const Menu = ({ menu }: { menu: TMenu[] }) => {
     <div>
       <h3 className={styles.title}>Menu</h3>
       {menu.map(({ id, ...dish }) => (
-        <Dish key={id} dish={dish} />
+        <Dish key={id} dish={dish} className={styles.dish} />
       ))}
     </div>
   )
