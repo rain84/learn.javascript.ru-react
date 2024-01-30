8c7444cda9a187d18352d0d9974abc31fae007b3
rain84 <rainy_sky@mail.ru>
2024-01-31 00:54:41 +0300

    feat: ✨ added <Review-Form>

--- src/components/restaurant/component.tsx
+++ src/components/restaurant/component.tsx
@@ -1,4 +1,6 @@
-import { type TRestaurant } from 'constants/restaurant.types'
+import { ReviewForm } from 'components/review-form'
+import type { TRestaurant, TReview } from 'constants/restaurant.types'
+import { useState } from 'react'
 import { Menu } from '../menu'
 import { Reviews } from '../reviews'
 import styles from './style.module.scss'
@@ -6,13 +8,19 @@ import styles from './style.module.scss'
 export const Restaurant = ({
   name,
   menu,
-  reviews,
+  reviews: reviews_,
 }: Omit<TRestaurant, 'id'>) => {
+  const [reviews, setReviews] = useState(reviews_)
+
+  const onSubmit = (data: TReview): void =>
+    setReviews((prev) => [...prev, data])
+
   return (
     <section className={styles.root}>
       <h1>Name: {name}</h1>
       <Menu menu={menu} />
       <Reviews reviews={reviews} />
+      <ReviewForm className={styles.review_form} onSubmit={onSubmit} />
     </section>
   )
 }
