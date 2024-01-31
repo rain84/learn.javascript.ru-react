98f1ebd70fa9aa2ec940da76f4dbab7ef538411a
rain84 <rainy_sky@mail.ru>
2024-01-31 22:04:12 +0300

    feat: ✨ added behavior to change rating for the <Rating> component

--- src/components/rating/component.tsx
+++ src/components/rating/component.tsx
@@ -1,12 +1,53 @@
+import clsx from 'clsx'
+import { useState } from 'react'
 import styles from './style.module.scss'
-import { TReview } from 'constants/restaurant.types'
 
-export const Rating = ({ rating }: TProps) => {
+export const Rating = ({
+  rating = 0,
+  canChange = false,
+  size,
+  onChange,
+}: TProps) => {
+  const [hoveredIndex, setHoveredIndex] = useState(-1)
+  const [isMouseOver, setIsMouseOver] = useState(false)
+
   return (
-    <span className={styles.root}>{Array(rating).fill('⭐️').join(' ')}</span>
+    <span
+      className={clsx(styles.root, canChange && styles.cursor_pointer)}
+      onMouseLeave={() => (setHoveredIndex(-1), setIsMouseOver(false))}
+      onMouseOver={() => setIsMouseOver(true)}
+    >
+      {Array(size)
+        .fill(0)
+        .map((_, index) => (
+          <span
+            key={index}
+            className={clsx(
+              styles.star,
+
+              // set class for the "rating"-star
+              (!canChange || (canChange && !isMouseOver)) &&
+                index < rating &&
+                styles.star_active,
+
+              // set class for the hovered-stars
+              canChange && index <= hoveredIndex && styles.star_active
+            )}
+            onMouseEnter={() => canChange && setHoveredIndex(index)}
+            onMouseLeave={() => canChange && setHoveredIndex(index - 1)}
+            onClick={() => canChange && onChange?.(index + 1)}
+          >
+            ⭐️
+          </span>
+        ))}
+    </span>
   )
 }
 
 type TProps = {
-  rating: TReview['rating']
+  rating: number
+  size: number
+  canChange?: boolean
+
+  onChange?: (rating: number) => void
 }
