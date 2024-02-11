70d381f0875c2dd0c50bb528b1b15e13176dac40
rain84 <rainy_sky@mail.ru>
2024-02-11 21:30:52 +0300

    feat: ✨ added selector for total price

--- src/redux_/ui/cart/selectors.ts
+++ src/redux_/ui/cart/selectors.ts
@@ -5,6 +5,15 @@ const selectModule = (state: TRootState) => state.cart
 const selectProductAmountById = (id: string) => (state: TRootState) =>
   selectModule(state)[id] || 0
 
+const selectTotalPrice = (state: TRootState) => {
+  const dishes = state.dish.entities
+  const totalPrice = Object.keys(selectModule(state)).reduce(
+    (acc, id) => acc + dishes[id].price,
+    0
+  )
+  return totalPrice
+}
+
 const selectTotalAmount = (state: TRootState) =>
   selectModule(state)
     ? Object.values(selectModule(state)).reduce(
@@ -17,4 +26,5 @@ export const cartSelectors = {
   selectProductAmountById,
   selectTotalAmount,
   selectModule,
+  selectTotalPrice,
 }
